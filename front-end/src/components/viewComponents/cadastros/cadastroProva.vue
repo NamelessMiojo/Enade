0f4380e1cc91d9c3c5583d8c69d6f1ca5e5ba3ea
Arthur Torres <54257895+NamelessMiojo@users.noreply.github.com>
2023-07-12 21:28:28 -0300

    Update cadastroProva.vue

--- front-end/src/components/viewComponents/cadastros/cadastroProva.vue
+++ front-end/src/components/viewComponents/cadastros/cadastroProva.vue
@@ -3,24 +3,24 @@
     <section>
         <section>
             <section>
-                <form ref="form" @submit.prevent="addProva">
+                <form ref="form" @submit.prevent>
                     <div>
                         <label for="data">Data</label>
                         <input type="text" id="data" v-model="newProva.dataProva"/>
                     </div>
-                    <div>
+                    <!--<div>
                         <label for="data">nome</label>
                         <input type="text" id="data" v-model="newProva.nome"/>
-                    </div>
+                    </div>-->
                     <div>
                         <label for="questoes">Questoes</label>
-                        <div v-for="questao in listQuestoes" :key="questao.id">
+                        <div v-for="questao in listQuestoes" :key="questao.id" >
                             {{questao.tipoQuestao.nome}} | 
                             {{questao.descricao}}
-                            <button @click="adicionarQuestao(questao.id)">Adicionar</button>
+                            <button @click="adicionarQuestao(questao)">Adicionar</button>
                         </div>
                     </div>
-                    <button @click="resetForm" > Cadastrar Prova </button>
+                    <button @click="addProva"> Cadastrar Prova </button>
                 </form>
 
             </section>
@@ -41,15 +41,12 @@ import axios from 'axios'
                 url: 'http://localhost:56918/prova/',
                 newProva:{
                     dataProva:'',
-                    nome:'',
                     questoes:[
                         
-                    ],
+                    ]
                 },
                 listQuestoes: [],
-                newQuestao:{
-                    id:'',
-                },
+                listProvaQuestoes:[],
                 
                 
             }
@@ -64,7 +61,9 @@ import axios from 'axios'
                 
             },
             addProva(){
+                console.log(this.newProva)
                 axios.post(this.url, this.newProva)
+                this.$refs.form.reset()
             }
         },
         mounted(){
